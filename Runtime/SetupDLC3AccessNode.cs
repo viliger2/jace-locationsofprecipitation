08a0805790c9185746c4fcc33ec9cddb9b178202
JaceCascade <66852270+JaceDaDorito@users.noreply.github.com>
2025-12-14 04:37:16 -0500

    start of access node support

---
+++ Runtime/SetupDLC3AccessNode.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupDLC3AccessNode : MonoBehaviour
+{
+    private static string accessNodePortalsGUID = "154892877f36e534fa0058ed8e10c63f";
+    private static string accessNodeGUID = "383d1a1eefe41db4eb8708719f86fc03";
+}
