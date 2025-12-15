95ff99f77542e89e5a515bd28b5dd4befc094938
JaceCascade <66852270+JaceDaDorito@users.noreply.github.com>
2025-12-14 22:39:40 -0500

    ditched the menu item, bit the bullet and made a custom asset for the mission controller

---
+++ Runtime/Code/InjectExpansion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace LOP
+{
+    /// <summary>
+    /// Component that injects an AddressableAsset to a component's field
+    /// </summary>
+    [ExecuteAlways]
+    public class InjectExpansion : MonoBehaviour
+    { }
+}
--- Runtime/Code/LocationsOfPrecipitation.cs
+++ Runtime/Code/LocationsOfPrecipitation.cs
@@ -3,6 +3,12 @@ using RoR2;
 using UnityEngine;
 using Path = System.IO.Path;
 using BepInEx.Logging;
+using System.Security.Permissions;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
 
 namespace LOP
 {
@@ -21,7 +27,7 @@ namespace LOP
         public static PluginInfo PluginInfo { get; private set; }
         internal static new ManualLogSource Logger { get; set; }
         private static string AssemblyDir { get => (Path.GetDirectoryName(PluginInfo.Location)); }
-
+        public static AssetBundle lopAssetBundle { get; private set; }
         private void Awake()
         {
             Instance = this;
@@ -29,6 +35,8 @@ namespace LOP
 
             Logger = base.Logger;
             new LOPLog(Logger);
+
+            lopAssetBundle = AssetBundle.LoadFromFile(Path.Combine(AssemblyDir, "runtimelopassetbundle"));
         }
     }
 }
